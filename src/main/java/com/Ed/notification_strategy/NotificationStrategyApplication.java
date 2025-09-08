bdbea5a67d6acf522576b4978a1a02b6f3da0642
Eduardo <eduardotigoulart@gmail.com>
2025-09-07 22:19:11 -0300

    Initial commit

---
+++ src/main/java/com/Ed/notification_strategy/NotificationStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.Ed.notification_strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationStrategyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ed/notification_strategy/NotificationStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ed.notification_strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
